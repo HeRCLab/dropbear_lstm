56c495958e168cf0cd20f3b547d2f2f02ceabb04
Philip Conrad <philipaconrad@gmail.com>
2020-02-03 01:26:39 -0500

    Add Python signal generator script.

---
+++ wavegen.py
@@ -0,0 +1,41 @@
+# Sine wave generator/mixer.
+
+import numpy as np
+from scipy import signal as sg
+import matplotlib.pyplot as plt
+
+# Wave properties.
+sampling_rate = 16000 # Sample rate
+freqs = [1, 3, 5, 10] # Frequency (Hz)
+num_samples = 16000
+
+# Other globals.
+x = np.arange(num_samples) # x-axis for the plots.
+
+
+if __name__ == '__main__':
+    # Build a dictionary of arrays of the form:
+    # {frequency: [x1, x2, x3, x4, ..., xn]}
+    y = {}
+    for freq in freqs:
+        y[freq] = 100 * np.sin(2 * np.pi * freq * x / sampling_rate)
+
+    # Plot subplot for each signal.
+    fig, axs = plt.subplots(len(freqs)+1, 1, sharex=True)
+    for (i, freq) in zip(range(len(freqs)+1), freqs):
+        axs[i].plot(x, y[freq], '-')
+        axs[i].set_ylabel('{} Hz'.format(freq))
+
+    # Create combined signal.
+    combined = np.zeros(num_samples)
+    for freq in freqs:
+        combined += y[freq]
+
+    # Plot combined signal.
+    axs[len(freqs)].plot(x, combined, '-')
+    axs[len(freqs)].set_ylabel('Combined'.format(freq))
+
+    # Put a title on the plot and the window, then render.
+    fig.suptitle('Combining signals at different frequencies', fontsize=15)
+    fig.canvas.set_window_title('Signals')
+    plt.show()

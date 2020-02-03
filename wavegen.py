a5303b1cd3137ba1f0e6ec38531a0f617732f734
Philip Conrad <philipaconrad@gmail.com>
2020-02-03 13:51:15 -0500

    Add a CLI to the wavegen script.
    This commit adds a command line interface (CLI) to the wavegen script.
    As part of those changes, plotting is now optional (using the `-p` option),
    and JSON export occurs first, since that should be very fast in most
    cases.

--- wavegen.py
+++ wavegen.py
@@ -1,47 +1,39 @@
 # Sine wave generator/mixer.
 import json
 import datetime
+import argparse
 import numpy as np
 from scipy import signal as sg
-import matplotlib.pyplot as plt
-
-# Wave properties.
-sampling_rate = 16000 # Sample rate
-freqs = [1, 3, 5, 10] # Frequency (Hz)
-num_samples = 16000
-
-# Other globals.
-x = np.arange(num_samples) # x-axis for the plots.
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("sample_rate", type=int, help="Sampling rate. (Hz)")
+    parser.add_argument("num_samples", type=int, help="Number of samples to generate.")
+    parser.add_argument("freqs", nargs='+', type=int, help="Frequencies to generate. (Hz)")
+    parser.add_argument("-p", "--plot", action="store_true", help="Plot using matplotlib.")
+
+    args = parser.parse_args()
+    #print(args)  # DEBUG
+
+    sampling_rate = args.sample_rate
+    num_samples = args.num_samples
+    freqs = args.freqs
+
+    x = np.arange(num_samples) # x-axis for the plots.
+
     # Build a dictionary of arrays of the form:
     # {frequency: [x1, x2, x3, x4, ..., xn]}
     y = {}
     for freq in freqs:
         y[freq] = 100 * np.sin(2 * np.pi * freq * x / sampling_rate)
 
-    # Plot subplot for each signal.
-    fig, axs = plt.subplots(len(freqs)+1, 1, sharex=True)
-    for (i, freq) in zip(range(len(freqs)+1), freqs):
-        axs[i].plot(x, y[freq], '-')
-        axs[i].set_ylabel('{} Hz'.format(freq))
-
     # Create combined signal.
     combined = np.zeros(num_samples)
     for freq in freqs:
         combined += y[freq]
 
-    # Plot combined signal.
-    axs[len(freqs)].plot(x, combined, '-')
-    axs[len(freqs)].set_ylabel('Combined'.format(freq))
-
-    # Put a title on the plot and the window, then render.
-    fig.suptitle('Combining signals at different frequencies', fontsize=15)
-    fig.canvas.set_window_title('Signals')
-    plt.show()
-
-    # Export data as JSON.
+    # Dump data as JSON to stdout.
     out = {
         "date_time": str(datetime.datetime.now(datetime.timezone.utc)),
         "acceleration_data": list(combined),
@@ -50,3 +42,22 @@ if __name__ == '__main__':
         "time_acceleration_data": [float(y) for y in x],
     }
     print(json.dumps(out))
+
+    # Plot only if the user asked for plotting.
+    if args.plot:
+        import matplotlib.pyplot as plt  # Intentionally late import.
+
+        # Plot subplot for each signal.
+        fig, axs = plt.subplots(len(freqs)+1, 1, sharex=True)
+        for (i, freq) in zip(range(len(freqs)+1), freqs):
+            axs[i].plot(x, y[freq], '-')
+            axs[i].set_ylabel('{} Hz'.format(freq))
+
+        # Plot combined signal.
+        axs[len(freqs)].plot(x, combined, '-')
+        axs[len(freqs)].set_ylabel('Combined'.format(freq))
+
+        # Put a title on the plot and the window, then render.
+        fig.suptitle('Combining signals at different frequencies', fontsize=15)
+        fig.canvas.set_window_title('Signals')
+        plt.show()

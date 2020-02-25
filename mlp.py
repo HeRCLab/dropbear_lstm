286c4c86fc6c45f6ede330ed0fb1540e506e1fe3
Philip Conrad <philipaconrad@gmail.com>
2020-02-25 12:50:00 -0500

    Add prediction + plotting code to MLP/ANN script.

--- mlp.py
+++ mlp.py
@@ -19,11 +19,13 @@ import tensorflow as tf     # issue with importing tensorflow on
 
 def parse_args():
     parser = argparse.ArgumentParser(description='ANN/MLP Model')
+    parser.add_argument('-e', '--epochs', type=int, default=10, help="Number of epochs to spend training the network. (default: 10)")
     parser.add_argument('-u', '--units', action="append", default=[1], help="Number of units in hidden layer. Providing this argument more than once will add additional hidden layers. (default: 1)")
     parser.add_argument('--history', '--history-length', type=int, default=10, help="Number of samples in the history buffer. (default: 10)")
     parser.add_argument('-w', '--training-window', type=int, default=40, help="Number of samples in each training window. (default: 40)")
     parser.add_argument('-f', '--filename', type=str, help="Filename of JSON waveform data to read in.")
     parser.add_argument('-t', '--training-ratio', type=float, help="Ratio of incoming data to use for training, on a scale of 0.0-1.0 (default: 0.1)")
+    parser.add_argument('-p', '--plot', action="store_true", help="Plot using matplotlib. (default: False)")
 
     return parser.parse_args()
 
@@ -43,7 +45,7 @@ def create_model(units, history_length):
     else:
         model.add(Dense(units, input_shape=(history_length,)))
     model.add(Dense(1))
-    model.compile(loss='mse',optimizer='adam')
+    model.compile(loss='mean_squared_error', optimizer='adam')
     return model
 
 
@@ -64,8 +66,7 @@ def train_model(units, x_train, y_train, history_length, epochs=10, want_gpu=Fal
         model = create_model(units, history_length)
         model.fit(x_train, y_train, epochs=epochs, verbose=0)
 
-    y_train_pred_norm = model.predict(x_train)
-    return y_train_pred_norm
+    return model
 
 
 def main():
@@ -92,10 +93,15 @@ def main():
     # make sure to reset x and y since we modify them for resampling
     x = np.array(data["acceleration_data"])
 
+    epochs = args.epochs
     units = [int(x) for x in args.units]
     prediction_time = 1
     training_window = args.training_window
     history_length = args.history
+    prev_model = None
+
+    y_predicted = [0 for x in range(0, training_window)] # Pack with zeros for first training window.
+    y_autopredicted = [0 for x in range(0, training_window)] # Pack with zeros for first training window.
 
     # Online training fun
     print("Number of windows: {}".format(len(range(0, len(x)-prediction_time-training_window, training_window))))
@@ -105,6 +111,7 @@ def main():
         x_train = []
         y_train = []
 
+        # Prepare training data.
         for j in range(0, training_window):
             start_idx = i - history_length + j
             if start_idx < 1:
@@ -115,11 +122,24 @@ def main():
         x_train = np.array(x_train)
         y_train = np.array(y_train)
 
+        # PREDICTION (Normal)
+        # Prediction across this buffer of samples, using the previous window's model.
+        if prev_model is not None:
+            for i in range(0, training_window):
+                future_x = np.array([x_train[i]])
+                y_pred = prev_model.predict([future_x])
+                #print("Prediction: {}".format(y_pred))
+                y_predicted += y_pred.flatten().tolist()
+
         # Normalize the training data.
         # TODO
 
         # Train the model + get predictions.
-        y_pred = train_model(units, x_train, y_train, history_length)
+        model = train_model(units, x_train, y_train, history_length, epochs=epochs)
+        print("x_train shape: {}".format(x_train.shape))
+        y_pred = model.predict(x_train)
+
+        prev_model = model # Swap in the last window's model.
         
         # the following line causes a runtime memory error, so I needed to expand the statement into a loop
         #rmse = math.sqrt(np.mean(np.square(y_train_pred - y_train)))
@@ -130,6 +150,49 @@ def main():
             diffs[i] = np.square(y_pred[i] - y_train[i])
         rmse = math.sqrt(np.mean(diffs))
         print("RMSE {}".format(rmse))
+
+        # AUTO-PREDICTION
+        # Attempt to predict the entire next window by iteratively predicting forward in time.
+        future_x = np.array([x_train[-1]])
+        j = 0
+        for i in range(0, training_window):
+            y_pred = model.predict([future_x])
+            #print("Prediction: {}".format(y_pred))
+            future_x = np.hstack((future_x[:,1:], y_pred)) # Move the window up 1 sample.
+            j += 1
+            # Append predicted samples to history list.
+            if j == history_length:
+                y_autopredicted += future_x.flatten().tolist()
+                j = 0
+
+    y_predicted += [0 for x in range(0, training_window)]
+
+    # Plot only if the user asked for plotting.
+    if args.plot:
+        import matplotlib.pyplot as plt  # Intentionally late import.
+
+        #num_plots = 3
+        num_plots = 2
+        x_data = np.arange(len(x))
+
+        # Plot subplot for each signal.
+        fig, axs = plt.subplots(num_plots, 1, sharex=True)
+        y_data = x
+        axs[0].plot(x_data, y_data, '-')
+        axs[0].set_ylabel('Signal')
+
+        y_data = y_predicted
+        axs[1].plot(x_data, y_data, '-')
+        axs[1].set_ylabel('Predicted (w/real data)')
+
+        #y_data = y_autopredicted
+        #axs[2].plot(x_data, y_data, '-')
+        #axs[2].set_ylabel('Auto-predicted')
+
+        # Put a title on the plot and the window, then render.
+        fig.suptitle('(MLP) Original vs Predicted signals', fontsize=15)
+        fig.canvas.set_window_title('MLP Predicted Signals')
+        plt.show()
 
 
 if __name__ == '__main__':

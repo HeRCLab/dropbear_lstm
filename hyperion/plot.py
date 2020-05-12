bf520999b8092a3c14d439cdb4acfff9a30b19a9
Philip Conrad <philipaconrad@gmail.com>
2020-05-12 13:50:11 -0400

    Update grid plot script to use SQLite databases.
    This commit changes the grid plot script to query from SQLite databases.
    Queries can be parameterized by the user on the command line, otherwise
    all data is plotted at once.

--- hyperion/plot.py
+++ hyperion/plot.py
@@ -1,20 +1,60 @@
+import sys
 import csv
+import argparse
+import sqlite3
 import matplotlib.pyplot as plt
 from collections import defaultdict
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 import numpy as np
 
-def get_data(filename):
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Grid plot tool for RTML project')
+
+    parser.add_argument("database", default="", help="Database file to query from.")
+    parser.add_argument("--where", action="append", default=[], help="WHERE constraints to add to the SQL query. Can be used multiple times to add more constraints. (Ex: '--where \"epochs >= 50\" ')")
+
+    return parser.parse_args()
+
+
+def get_data_sqlite(location, sql_query):
     out = defaultdict(list)
+    i = 0
+    conn = sqlite3.connect(location)
+    cursor = conn.cursor()
+
+    # Run the user-provided query, and fetch all rows.
+    cursor.execute(sql_query)
+    rows = cursor.fetchall()
+
+    # Build data dictionary.
+    # It's okay for this to be O(n^2), because performance doesn't matter here.
+    field_names = ["id","author","algorithm","activation","dataset","creation_ts","forecast_length","prediction_gap","training_window_length","sample_window_length","epochs","layers","rmse_global","metadata"]
+    for row in rows:
+        kv_pairs = zip(field_names, row)
+        for (k, v) in kv_pairs:
+            out[k].append(v)
+        i += 1
+
+    print("Fetched {} records from database '{}'".format(i, location))
+    out["num_records"] = i
+    return dict(out)
+
+
+def get_data_csv(filename):
+    out = defaultdict(list)
+    i = 0
     with open(filename, 'r') as f:
         reader = csv.DictReader(f)
         for row in reader:
             out["nodes"].append(int(row["nodes"]))
-            out["window_length"].append(int(row["window_length"]))
-            out["history_length"].append(int(row["history_length"]))
+            out["training_window_length"].append(int(row["training_window_length"]))
+            out["sample_window_length"].append(int(row["sample_window_length"]))
             out["epochs"].append(int(row["epochs"]))
-            out["rmse"].append(float(row["rmse"]))
+            out["rmse_global"].append(float(row["rmse_global"]))
+            i += 1
+    out["num_records"] = i
     return dict(out)
 
 
@@ -27,59 +67,68 @@ def gen_plots(d):
     fig = plt.figure()
 
     # Set axis labels.
-    for (y, text) in zip([0.75, 0.5, 0.25], ["Epochs", "Window\nlength", "History\nlength"]):
+    for (y, text) in zip([0.75, 0.5, 0.25], ["Epochs", "Training Window\nlength", "Sample Window\nlength"]):
         fig.text(0.06, y, text, ha='center', va='center', rotation=90)
-    for (x, text) in zip([0.25, 0.5, 0.75], ["Epochs", "Window length", "History length"]):
+    for (x, text) in zip([0.25, 0.5, 0.75], ["Epochs", "Training Window length", "Sample Window length"]):
         fig.text(x, 0.9, text, ha='center', va='center')
 
     # --------------------------------------------------------------
     # First row
     axs = fig.add_subplot(3, 3, 2, projection='3d')
-    axs.scatter(d['window_length'], d['epochs'], d['rmse'])
-    axs.set_xlabel('Window length')
+    axs.scatter(d['training_window_length'], d['epochs'], d['rmse_global'])
+    axs.set_xlabel('Training Window length')
     axs.set_ylabel('Epochs')
     axs.set_zlabel('RMSE')
 
     axs = fig.add_subplot(3, 3, 3, projection='3d')
-    axs.scatter(d['history_length'], d['epochs'], d['rmse'])
-    axs.set_xlabel('History length')
+    axs.scatter(d['sample_window_length'], d['epochs'], d['rmse_global'])
+    axs.set_xlabel('Sample Window length')
     axs.set_ylabel('Epochs')
     axs.set_zlabel('RMSE')
 
     # --------------------------------------------------------------
     # Second row
     axs = fig.add_subplot(3, 3, 4, projection='3d')
-    axs.scatter(d['epochs'], d['window_length'], d['rmse'])
+    axs.scatter(d['epochs'], d['training_window_length'], d['rmse_global'])
     axs.set_xlabel('Epochs')
-    axs.set_ylabel('Window length')
+    axs.set_ylabel('Training Window length')
     axs.set_zlabel('RMSE')
 
     axs = fig.add_subplot(3, 3, 6, projection='3d')
-    axs.scatter(d['history_length'], d['window_length'], d['rmse'])
-    axs.set_xlabel('History length')
-    axs.set_ylabel('Window length')
+    axs.scatter(d['sample_window_length'], d['training_window_length'], d['rmse_global'])
+    axs.set_xlabel('Sample Window length')
+    axs.set_ylabel('Training Window length')
     axs.set_zlabel('RMSE')
 
     # --------------------------------------------------------------
     # Third row
     axs = fig.add_subplot(3, 3, 7, projection='3d')
-    axs.scatter(d['epochs'], d['history_length'], d['rmse'])
+    axs.scatter(d['epochs'], d['sample_window_length'], d['rmse_global'])
     axs.set_xlabel('Epochs')
-    axs.set_ylabel('History length')
+    axs.set_ylabel('Sample Window length')
     axs.set_zlabel('RMSE')
 
     axs = fig.add_subplot(3, 3, 8, projection='3d')
-    axs.scatter(d['window_length'], d['history_length'], d['rmse'])
-    axs.set_xlabel('Window length')
-    axs.set_ylabel('History length')
+    axs.scatter(d['training_window_length'], d['sample_window_length'], d['rmse_global'])
+    axs.set_xlabel('Training Window length')
+    axs.set_ylabel('Sample Window length')
     axs.set_zlabel('RMSE')
 
     # Put a title on the plot and the window, then render.
-    fig.suptitle('MLP/ANN Batch Run Results', fontsize=15)
-    fig.canvas.set_window_title('MLP Batch Run Results')
+    fig.suptitle('MLP/ANN Batch Run Results ({} records)'.format(d['num_records']), fontsize=15)
+    fig.canvas.set_window_title('MLP Batch Run Results ({} records)'.format(d['num_records']))
     plt.show()
 
 
 if __name__ == "__main__":
-    data_dict = get_data('collation.csv')
+    sql = "SELECT * FROM AlgorithmResult"
+    args = parse_args()
+
+    # User can inject arbitrary SQL here, but we never commit the results, so
+    # it should be safe to allow here.
+    if len(args.where) > 0:
+        sql += " WHERE "
+        sql += " AND ".join(args.where)
+
+    data_dict = get_data_sqlite(args.database, sql)
     gen_plots(data_dict)

b2f0a3f675ded9a6c665fad4c85f221fd844620d
U-JASONLAPTOP4\Jason D. Bakos <jbakos@cse.sc.edu>
2020-06-18 11:44:42 -0400

    matlab model is working

--- make_signal.m
+++ make_signal.m
@@ -5,7 +5,7 @@ function [x,signal] = make_signal (sample_rate,time,amps,freqs,phases)
 
     signal = zeros(1,size(x,2));
     
-    for i=size(amps,2)
+    for i=1:size(amps,2)
         amplitude = amps(i);
         frequency = freqs(i);
         phase = phases(i);
